cebcff038ec2f27948e0b22d2a1ab77cf154a40c
Gailo55 <s2221053@cco.kanagawa-it.ac.jp>
2024-08-26 23:33:16 +0900

    Delete toa/qr_maker.dart

--- toa/qr_maker.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-
-QrImage(
-  data: 'https://www.kamo-it.org/blog/36/',
-  version: QrVersions.auto,
-  ize: 200.0,
-)
