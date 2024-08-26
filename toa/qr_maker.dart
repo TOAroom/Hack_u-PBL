52a93ae071c4ed98d7247a5cd0e937623ae94196
Gailo55 <s2221053@cco.kanagawa-it.ac.jp>
2024-08-26 23:32:09 +0900

    Add files via upload

---
+++ toa/qr_maker.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+QrImage(
+  data: 'https://www.kamo-it.org/blog/36/',
+  version: QrVersions.auto,
+  ize: 200.0,
+)
