82cd2303c8a807e14f7de22efc290c6eef387d90
yokonag <shon1019@softbank.ne.jp>
2024-08-27 18:42:53 +0900

    ファイルをきれいにしてみた

--- lib/customer_OR_store/customer_page_main/page_create/create_card_main/create_card_main.dart
+++ lib/customer_OR_store/customer_page_main/page_create/create_card_main/create_card_main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'card_list_page.dart';
 
 class CreateCardMain extends StatelessWidget {
+  // 戻るボタンを押したとき
+  back(BuildContext context) {
+    // 前の画面 へ戻る
+    context.pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
