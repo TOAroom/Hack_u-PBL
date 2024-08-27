ecf581104fba3e76db26eb84b07676415ffc587f
yokonag <shon1019@softbank.ne.jp>
2024-08-27 12:29:57 +0900

    客と店の選択画面実装

---
+++ toa/ryuunosuke_point_card/customer_OR_store.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:bordered_text/bordered_text.dart';
+
+class Customer_OR_Store extends StatelessWidget {
+  Customer_OR_Store({super.key});
+
+  //戻るボタン押したとき
+  void back(BuildContext context) {
+    context.pop();
+  }
+
+  void navigateTo(BuildContext context, String route) {
+    context.push(route);
+  }
+
+  ButtonStyle style = ElevatedButton.styleFrom(
+    backgroundColor: Colors.blue, //ボタンの背景色の指定
+    foregroundColor: Colors.white, //ボタンの縁取り
+    elevation: 5,
+    padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 100),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(0.0), // 任意の角丸さを指定
+    ),
+    side: const BorderSide(color: Color.fromARGB(255, 3, 10, 15)),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 94, 199, 73),
+        title: const Text(
+          'ポケっとかーど',
+          style: TextStyle(
+              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            //客側への画面遷移
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.face,
+                color: Colors.white,
+                size: 40,
+              ),
+              label: BorderedText(
+                strokeWidth: 2.0,
+                strokeColor: Colors.black,
+                child: const Text(
+                  'CUSTOMER',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              onPressed: () => navigateTo(context, '/main'),
+              style: style,
+            ),
+            //店側への画面遷移
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.home,
+                color: Colors.white,
+                size: 40,
+              ),
+              label: BorderedText(
+                strokeWidth: 2.0,
+                strokeColor: Colors.black,
+                child: const Text(
+                  'STORE',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              onPressed: () => navigateTo(context, '/s_main'),
+              style: style,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
